6b020cbb556251cd2cf974331018f3304d382533
Gen Takayama <takayama@ne.phys.sci.osaka-u.ac.jp>
2024-10-20 21:39:00 +0900

    comment

--- WEB/JSR/JSROOT/20241017.pl
+++ WEB/JSR/JSROOT/20241017.pl
@@ -18,7 +18,7 @@ my $runNo = $cgi->param('runNo') || '';
 my $runName = $cgi->param('runName') || '';
 
 # Define the directory path and construct the file name
-my $dir = "/EXP/RIBF/TRIP/2024/AUTUMN/USR/$default/$hist_or_tree/";
+my $dir = "/fuji/EXP/RIBF/TRIP/2024/AUTUMN/USR/$default/$hist_or_tree/";
 my $file = "${runName}${runNo}.root";
 
 # Output the HTML
@@ -30,7 +30,7 @@ print << "HTML";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Read a ROOT file</title>
     <link rel="shortcut icon" href="img/RootIcon.ico"/>
-    <script type="text/javascript" src="/JSROOT/scripts/JSRootCore.js?gui"></script>
+    <script type="text/javascript" src="/fuji/JSROOT/scripts/JSRootCore.js?gui"></script>
 </head>
 <body>
     <div id="simpleGUI" path="$dir" files="$file"></div>
--- WEB/RIDF/20241020.php
+++ WEB/RIDF/20241020.php
@@ -19,7 +19,7 @@ if (($handle = fopen($csvFile, 'r')) !== false) {
     $headers = fgetcsv($handle, 1000, ',', '"');
 
     // ヘッダーに ridf2root と JSROOT を追加
-    array_unshift($headers, 'ridf2root (10k)', 'JSROOT');
+    array_unshift($headers, 'ridf2root (イベント数を選択)', 'JSROOT');
 
     // CSSスタイルの追加
     echo "<style>
@@ -73,11 +73,67 @@ if (($handle = fopen($csvFile, 'r')) !== false) {
                 border: 1px solid #ccc;
                 border-radius: 4px;
             }
+	    .update_button {
+	        display: inline-block;
+	        padding: 10px 20px;
+	        font-size: 16px;
+	        color: white;
+	        background-color: #4CAF50; /* ボタンの色 */
+	        border: none;
+	        border-radius: 5px; /* 角を丸く */
+	        text-align: center;
+	        text-decoration: none; /* 下線を消す */
+	        transition: background-color 0.3s, transform 0.3s; /* ホバー効果のトランジション */
+	    }
+	    
+	    .update_button:hover {
+	        background-color: #45a049; /* ホバー時の色 */
+	        transform: scale(1.05); /* ホバー時のサイズ変更 */
+	    }
+	    .exe-button {
+	        background-color: #EB9899; /* 薄い赤色 */
+	        border: none;
+	        color: white;
+	        padding: 1px 2px;
+	        text-align: center;
+	        text-decoration: none;
+	        display: inline-block;
+	        font-size: 16px;
+	        margin: 1px 2px;
+	        transition-duration: 0.4s;
+	        cursor: pointer;
+	        border-radius: 4px;
+	    }
+	    .exe-button:hover {
+	        background-color: white;
+	        color: black;
+	        border: 2px solid #EB9899;
+	    }
+	    .button {
+	        display: inline-block;
+	        padding: 10px 20px;
+	        font-size: 16px;
+	        color: white;
+	        background-color: #4CAF50; /* ボタンの色 */
+	        border: none;
+	        border-radius: 5px; /* 角を丸く */
+	        text-align: center;
+	        text-decoration: none; /* 下線を消す */
+	        transition: background-color 0.3s, transform 0.3s; /* ホバー効果のトランジション */
+	    }
+	    .cool-button:hover {
+	        background-color: white;
+	        color: black;
+	        border: 2px solid #4CAF50;
+	    }
           </style>";
 
     // タイトルとリンクを表示
-    echo "<h2>ridf data list ( TRIP-S3CAN 2024 Autumn )</h2>";
-    echo "<p><a href='/cgi-bin/$base_url/EXP/RIBF/TRIP/2024/AUTUMN/USR/default/RIDF/csvruninfo.pl' style='color: #4CAF50; text-decoration: none;'>Update runsummary</a></p>";
+    echo "<h2>TRIP-S3CAN 2024秋 取得データの一覧</h2>";
+    #echo "<p><a href='/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/RIDF/csvruninfo.pl' style='color: #4CAF50; text-decoration: none;'>Update runsummary</a></p>";
+    echo "<div style='display: flex; justify-content: center; align-items: center;'>
+          <a href='/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/default/RIDF/csvruninfo.pl' class='update_button'>一覧表を更新する</a>
+      </div>";
 
     // HTMLテーブルの開始
     echo "<table id='dataTable'>";
@@ -105,11 +161,32 @@ if (($handle = fopen($csvFile, 'r')) !== false) {
         // Run nameを取得
         $runName = $data[0]; // Assuming the Run name is in the first column
 
-        // ridf2root URLを最初の列に追加
-        echo "<td><a href='/cgi-bin/$base_url/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/ridf2root?runNo=$runNumber&runName=" . urlencode($runName) . "' target='_blank'>ridf2root</a></td>";
+        //// ridf2root URLを最初の列に追加
+        //echo "<td><a href='/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/ridf2root?runNo=$runNumber&runName=" . urlencode($runName) . "' target='_blank'>ridf2root</a></td>";
 
-        // JSROOT URLを次の列に追加
-        echo "<td><a href='/cgi-bin/$base_url/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/WEB/JSR/jsroot.pl?runNo=$runNumber&runName=" . urlencode($runName) . "' target='_blank'>HIST</a>, <a href='/cgi-bin/$base_url/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/WEB/JSR/jsroot.pl?hist_or_tree=ROOT&runNo=$runNumber&runName=" . urlencode($runName) . "' target='_blank'>TREE</a></td>";
+	// ridf2root URLをボタンに置き換え、数値に変換してURLを作成
+	$loadEvts = ['10k' => 10000, '100k' => 100000, '1M' => 1000000, '1G' => 1000000000];
+	
+	// 現在のURLを取得
+	$currentUrl = $_SERVER['PHP_SELF'];
+	
+	echo "<td>";
+	if (strpos($currentUrl, 'ridf.php') !== false) {
+	    // ridf.phpの場合、「10k」ボタンのみを表示
+	    echo "<button class='exe-button' type='button' onclick=\"window.open('/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/web_ridf2root?runNo=$runNumber&runName=" . urlencode($runName) . "&loadEvts=" . $loadEvts['10k'] . "', '_blank')\">10k</button> ";
+	} elseif (strpos($currentUrl, 'ridf.auth.php') !== false) {
+	    // ridf.auth.phpの場合、全ボタンを表示
+	    foreach ($loadEvts as $label => $value) {
+	        echo "<button class='exe-button' type='button' onclick=\"window.open('/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/web_ridf2root?runNo=$runNumber&runName=" . urlencode($runName) . "&loadEvts=$value', '_blank')\">$label</button> ";
+	    }
+	}
+	echo "</td>";
+
+	// JSROOT URLを次の列に追加し、HISTとTREEをボタンに置き換え
+	echo "<td>";
+	echo "<button class='cool-button' type='button' onclick=\"window.open('/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/WEB/JSR/jsroot.pl?runNo=$runNumber&runName=" . urlencode($runName) . "', '_blank')\">HIST</button> ";
+	echo "<button class='cool-button' type='button' onclick=\"window.open('/$base_url/cgi-bin/EXP/RIBF/TRIP/2024/AUTUMN/USR/$subDir/WEB/JSR/jsroot.pl?hist_or_tree=ROOT&runNo=$runNumber&runName=" . urlencode($runName) . "', '_blank')\">TREE</button>";
+	echo "</td>";
 
         // 残りのデータを出力
         foreach ($data as $cell) {
